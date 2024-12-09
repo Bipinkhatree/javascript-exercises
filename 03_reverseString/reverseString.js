0baa7ae94e5f75127c8ebb934eebf78f5cb85435
Bipin Khatri <159134672+Bipinkhatree@users.noreply.github.com>
2024-12-09 12:14:22 +0545

    Complete 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    let finalStr = [];
+    let newStr = "";
+    const arrayOfString = str.split(" ");
+    for (let x = arrayOfString.length - 1; x >= 0; x--) {
+        for (let i = arrayOfString[x].length - 1; i >= 0; i--) {
+            newStr += arrayOfString[x][i];
+        }
+        finalStr.push(newStr);
+        newStr = "";
+    }
 
+    return finalStr.join(" ");
 };
 
 // Do not edit below this line
