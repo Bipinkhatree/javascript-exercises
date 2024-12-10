f5de327c5360ecd1b112289c10099be6b807277b
Bipin Khatri <159134672+Bipinkhatree@users.noreply.github.com>
2024-12-10 18:08:40 +0545

    Complete 5th Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (startIndx, endIndx) {
+    let sum = 0;
+    if (startIndx < 0 || endIndx < 0) {
+        return "ERROR";
+    }
+    else if (!Number.isInteger(startIndx) || !Number.isInteger(endIndx)) {
+        return "ERROR";
+    }
+    else if (typeof startIndx != 'number' || typeof endIndx != 'number') {
+        return "ERROR";
+    }
+    else if (startIndx < endIndx) {
+        for (i = startIndx; i <= endIndx; i++) {
+            sum = sum + i;
+        };
+    }
+    else
+        for (i = endIndx; i <= startIndx; i++) {
+            sum = sum + i;
+        };
+    return sum;
 };
 
 // Do not edit below this line
