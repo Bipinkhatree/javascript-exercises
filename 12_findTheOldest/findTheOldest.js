6d8c2fd0c5f6765fbf2f73ce3bde2c32ec209001
Bipin Khatri <159134672+Bipinkhatree@users.noreply.github.com>
2024-12-28 23:34:41 +0545

    Finally finish solving the findTheOldest problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
+// const findTheOldest = function (arr) {
+//     arr.sort(function (a, b) {
 
+//         if (!a.yearOfDeath) { a.yearOfDeath = new Date().getFullYear(); }
+
+//         if (!b.yearOfDeath) { b.yearOfDeath = new Date().getFullYear(); }
+
+//         let aAge = a.yearOfDeath - a.yearOfBirth;
+//         let bAge = b.yearOfDeath - b.yearOfBirth;
+//         return bAge - aAge;
+//     })
+//     return arr[0];
+
+// };
+
+
+//Now Lets try using other methods that are optimal
+
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
+
+const findTheOldest = function (people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+        return oldestAge < currentPersonAge ? currentPerson : oldest;
+    });
+}
+//
 // Do not edit below this line
 module.exports = findTheOldest;
