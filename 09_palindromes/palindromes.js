2f6a30def9519126771b5565236755f33c5def58
Bipin Khatri <159134672+Bipinkhatree@users.noreply.github.com>
2024-12-28 18:11:20 +0545

    Finish solving pallindrome problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let normalizedString = str.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    let reversedString = normalizedString.split('').reverse().join('');
+    if (normalizedString === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
